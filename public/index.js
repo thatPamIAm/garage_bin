580c9c57b46aed7bcd47d7ba5a5b3c05ef21605d
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 12:20:20 -0600

    Add addItem() and appendItem()

--- public/index.js
+++ public/index.js
@@ -4,10 +4,35 @@ $('.submit-button').on('click', (e) => {
   let $reason = $('.input-linger').val();
   let $cleanliness = $('.cleanliness-selector').val();
   clearInputs();
+  addItem($name, $reason, $cleanliness)
 });
 
 const clearInputs = () => {
     $('.input-name').val('')
     $('.input-linger').val('')
     $('.cleanliness-selector').val('')
+};
+
+const addItem = (name, reason, cleanliness) => {
+  fetch('/api/v1/junk', {
+    method: 'POST',
+    headers: { 'Content-type' : 'application/json' },
+    'body' : JSON.stringify({
+      name: name,
+      reason: reason,
+      cleanliness: cleanliness
+    })
+  })
+  .then(response => response.json())
+  .then((junk) => {
+    appendItem(junk.name)
+  })
+};
+
+const appendItem = (name) => {
+  $('.garage-list').append(`
+    <div>
+      <h3>${name}</h3>
+    </div>
+  `)
 };
