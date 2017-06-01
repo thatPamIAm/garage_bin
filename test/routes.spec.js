dd3d437a0c6b4a4f70406b527bdf4a8d9f045ebf
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 10:54:43 -0600

    Setup of beforeEach and afterEach blocks. Initial tests for client routes. 2 tests passing for client side." `

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,3 +9,50 @@ const configuration = require('../knexfile')['test'];
 const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
+
+
+describe('All the tests', () => {
+  before((done) => {
+    database.migrate.latest()
+    .then(() => {
+      return database.seed.run();
+    })
+    .then(() => {
+      done();
+    });
+  });
+
+  afterEach((done) => {
+    database.seed.run()
+    .then(() => {
+      done();
+    });
+  });
+
+  describe('Client Routes', () => {
+    it('should return homepage with html text', (done) => {
+      chai.request(server)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+
+        done();
+
+      });
+    });
+
+    it('should return a 404 for a non existent route', (done) => {
+      chai.request(server)
+      .get('/sosad')
+      .end((err, response) => {
+        response.should.have.status(404)
+        done()
+      })
+    })
+  })
+
+  describe('API Routes', () => {
+
+  })
+})
