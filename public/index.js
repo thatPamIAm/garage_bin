495dd1ca5f95823137a9fdf9c15a1e71b4566c77
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 12:10:47 -0600

    Add function to clear to form field inputs/selector.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,13 @@
 $('.submit-button').on('click', (e) => {
   e.preventDefault();
-  let $name = $('.input-name').val()
-  let $reason = $('.input-linger').val()
+  let $name = $('.input-name').val();
+  let $reason = $('.input-linger').val();
   let $cleanliness = $('.cleanliness-selector').val();
+  clearInputs();
 });
+
+const clearInputs = () => {
+    $('.input-name').val('')
+    $('.input-linger').val('')
+    $('.cleanliness-selector').val('')
+};
