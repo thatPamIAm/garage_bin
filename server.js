0910ad32540889080252625c100c3009ee3c96a7
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 11:07:41 -0600

    Add GET route for all the junk

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('.knexfile')[environment];
-const database = require('knex')[configuration];
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 
@@ -18,6 +18,19 @@ app.get('/', (request, response) => {
   });
 });
 
+// Get all of the junk
+app.get('/api/v1/junk', (request, response) => {
+  database('junk').select()
+    .then(junk => {
+      response.status(200).json(junk);
+    })
+    .catch(error => {
+      response.status(500).send({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Garage_bin is running on ${app.get('port')}.`)
 });
+
+module.exports = app;
