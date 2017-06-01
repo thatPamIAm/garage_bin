a0f99b29042d37f12709957622c73a062620d536
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 11:12:17 -0600

    Hardcode primary key for test seeds

--- db/seeds/test/junk.js
+++ db/seeds/test/junk.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       return Promise.all([
         knex('junk').insert([
-          { name: 'test shovel', reason: 'test digging holes', cleanliness: 'dusty'},
-          { name: 'test wheelbarrow', reason: 'test filling holes', cleanliness: 'rancid'}
+          { id: 1, name: 'test shovel', reason: 'test digging holes', cleanliness: 'dusty'},
+          { id: 2, name: 'test wheelbarrow', reason: 'test filling holes', cleanliness: 'rancid'}
         ])
       ]) //end return Promise.all
     });
