a7433bbb4038a17db2960fe703c6f23c83f28735
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 12:08:27 -0600

    Add click event to submit button to grab form field values.

---
+++ public/index.js
@@ -0,0 +1,6 @@
+$('.submit-button').on('click', (e) => {
+  e.preventDefault();
+  let $name = $('.input-name').val()
+  let $reason = $('.input-linger').val()
+  let $cleanliness = $('.cleanliness-selector').val();
+});
