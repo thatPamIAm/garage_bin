6040f7b2d3186eb74c2e0944ac59aa0fec33f175
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 09:59:53 -0600

    Add block for environment, knex config, and database config.

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.set('port', process.env.PORT || 3000)
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('.knexfile')[environment];
+const database = require('knex')[configuration];
+
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
   fs.readFile(`${__dirname}/index.html`, (err, file) => {
     response.send(file)
-  })
-})
+  });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Garage_bin is running on ${app.get('port')}.`)
-})
+});
