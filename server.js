eed4c66ee6cc468e47d7e95858dae53f169b11be
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-01 09:31:02 -0600

    Basic server.js file set up.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', (request, response) => {
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
